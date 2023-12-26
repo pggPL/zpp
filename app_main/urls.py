fa06eb3e6367e050e3256d77a98ec1f896d37508
Paweł Gadziński <62263673+pggPL@users.noreply.github.com>
2023-12-26 23:18:47 +0100

    under construction

--- app_main/urls.py
+++ app_main/urls.py
@@ -2,7 +2,9 @@
 from django.urls import path
 
 from . import views
+from .views import logout_view
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('logout/', logout_view, name='logout')
 ]
--- app_main/views.py
+++ app_main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
 
 # index.html
 
@@ -6,14 +8,29 @@ from django.shortcuts import render
 def index(request):
     # if user is not logger, show login screen
     if not request.user.is_authenticated:
-        return render(request, "app_main/login.html")
+        return login_view(request)
     return main_panel_view(request)
 
 def login_view(request):
-    return render(request, "app_main/login.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return main_panel_view(request)  # Redirect to homepage or other page
+        else:
+            # Invalid login
+            return render(request, 'app_main/login.html', {'error': 'Niepoprawne dane logowania'})
+    else:
+        return render(request, 'app_main/login.html')
 
 def main_panel_view(request):
     return render(request, "app_main/main_panel.html")
 
 def add_file_view(request):
     return render(request, "app_main/add_file.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("index")
