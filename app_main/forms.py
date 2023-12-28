4cc2c13e4d58e31f78a2837f9a80d9f767cb05fd
Paweł Gadziński <62263673+pggPL@users.noreply.github.com>
2023-12-28 18:34:53 +0100

    accounts

--- app_main/forms.py
+++ app_main/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from django.contrib.auth.forms import PasswordChangeForm
 from django.core.validators import FileExtensionValidator
 from django.forms import ModelForm, Select
 
-from app_main.models import Profile
+from app_main.models import Profile, Submission
 
 
 class FileUploadForm(forms.Form):
@@ -25,3 +26,42 @@ class ProfileForm(ModelForm):
         widgets = {
             'rank': Select(attrs={'class': 'js-example-basic-single'}),
         }
+    
+    def save(self, commit=True):
+        user = super(ProfileForm, self).save(commit=False)
+        if user.rank == "Senior":
+            user.is_staff = True
+        else:
+            user.is_staff = False
+        if commit:
+            user.save()
+        return user
+
+
+class LinkForm1(ModelForm):
+    class Meta:
+        model = Submission
+        fields = ['category']
+        labels = {
+            'category': 'Kategoria',
+        }
+        widgets = {
+            'category': Select(attrs={'class': 'js-example-basic-single'}),
+        }
+
+
+class LinkForm2(ModelForm):
+    class Meta:
+        model = Submission
+        fields = ['link', 'category', 'platform']
+        labels = {
+            'link': 'Link',
+            'category': 'Kategoria',
+            'platform': 'Platforma',
+        }
+
+class ChangePasswordForm(PasswordChangeForm):
+    # change labels
+    old_password = forms.CharField(label='Stare hasło', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    new_password1 = forms.CharField(label='Nowe hasło', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    new_password2 = forms.CharField(label='Powtórz nowe hasło', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
--- app_main/views.py
+++ app_main/views.py
@@ -1,16 +1,30 @@
+import os
+
 from django.contrib.auth import authenticate, login, logout
+import requests
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
-from app_main.forms import FileUploadForm, ProfileForm
+from app_main.forms import FileUploadForm, ProfileForm, LinkForm1, LinkForm2, ChangePasswordForm
 import openpyxl
 
-from app_main.models import Submission, Platform, Profile
+from app_main.models import Submission, Platform, Profile, SubmissionCategory
 from django.core.paginator import Paginator
+from selenium import webdriver
 
 
+# decorator – require senior rank
+def senior_required(view_func):
+    def wrapper(request, *args, **kwargs):
+        if request.user.rank == "Senior":
+            return view_func(request, *args, **kwargs)
+        else:
+            return HttpResponse("Nie masz uprawnień do wykonania tej akcji")
+    return wrapper
 
-# index.html
 
 
 def index(request):
@@ -33,8 +47,9 @@ def login_view(request):
     else:
         return render(request, 'app_main/login.html')
 
+@login_required
 def main_panel_view(request):
-    links_list = Submission.objects.all()
+    links_list = Submission.objects.all().order_by('link')
     
     # Paginacja
     paginator = Paginator(links_list, 30)  # 10 linków na stronę
@@ -47,6 +62,7 @@ def main_panel_view(request):
     
     return render(request, "app_main/main_panel.html", context={'links': links})
 
+@login_required
 def add_file_view(request):
     if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
@@ -54,31 +70,48 @@ def add_file_view(request):
             file = request.FILES['file']
             workbook = openpyxl.load_workbook(file)
             worksheet = workbook.active
+            
+            total_links = worksheet.max_row - 1
+            total_unique_links = 0
 
-            data = []
             for row in worksheet.iter_rows(min_row=2, max_col=2, values_only=True):
                 platform, link = row
+                
+                # check if link already exists in database
+                if Submission.objects.filter(link=link).exists():
+                    continue
+                total_unique_links += 1
                 
                 platform = Platform.objects.get_or_create(name=platform)[0]
                 
                 # create link in database
                 Submission.objects.get_or_create(link=link, platform=platform)
+            
+            context = {
+                'total_links': total_links,
+                'total_unique_links': total_unique_links,
+                'message': 'Plik został dodany. ',
+                'form': form
+            }
 
-            return render(request, "app_main/add_file.html", {'form': form, 'message': 'Plik został dodany'})
+            return render(request, "app_main/add_file.html", context)
     else:
         form = FileUploadForm()
     return render(request, "app_main/add_file.html", {'form': form})
 
+@login_required
 def logout_view(request):
     logout(request)
     return redirect("index")
 
+@senior_required
 def accounts_list_view(request):
     context = {
         'users': Profile.objects.all()
     }
     return render(request, "app_main/accounts.html", context=context)
 
+@senior_required
 def edit_account_view(request, pk):
     obiekt = get_object_or_404(Profile, pk=pk)
     if request.method == "POST":
@@ -90,6 +123,7 @@ def edit_account_view(request, pk):
         form = ProfileForm(instance=obiekt)
     return render(request, 'app_main/form.html', {'form': form, 'name': 'Edytuj konto'})
 
+@senior_required
 def delete_account_view(request, pk):
     # delete if there is no object connected to this account
     account = get_object_or_404(Profile, pk=pk)
@@ -99,6 +133,7 @@ def delete_account_view(request, pk):
     account.delete()
     return redirect('accounts_list')
 
+@senior_required
 def add_account_view(request):
     info_message = "Na podany adres email zostanie wysłane zaproszenie do rejestracji."
     if request.method == "POST":
@@ -110,3 +145,108 @@ def add_account_view(request):
         form = ProfileForm()
     return render(request, 'app_main/form.html',
                   {'form': form, 'name': 'Dodaj konto', 'back_link': 'accounts_list', 'info': info_message})
+
+@login_required
+def link_panel_view(request):
+    context = {
+        'links_with_forms': []
+    }
+    
+    submissions = Submission.objects.all()
+
+    for link in submissions:
+        link.short_link = link.link[:30] + "..." if len(link.link) > 30 else link.link
+    
+    for link in submissions:
+        form = LinkForm1(instance=link)
+        context['links_with_forms'].append({"form": form, "link": link, "done": link.category is not None})
+    # sort links_with_forms by done
+    context['links_with_forms'].sort(key=lambda x: x['done'])
+        
+    return render(request, "app_main/link_panel.html", context=context)
+
+@login_required
+def delete_link_view(request, pk, action):
+    link = get_object_or_404(Submission, pk=pk)
+    link.delete()
+    return redirect(action)
+
+@login_required
+def edit_link_view(request, pk, action):
+    obiekt = get_object_or_404(Submission, pk=pk)
+    if request.method == "POST":
+        form = LinkForm2(request.POST, instance=obiekt)
+        if form.is_valid():
+            form.save()
+            return redirect(action)
+    else:
+        form = LinkForm2(instance=obiekt)
+    return render(request, 'app_main/form.html', {'form': form, 'name': 'Edytuj link'})
+
+@login_required
+def add_link_view(request, action):
+    if request.method == "POST":
+        form = LinkForm2(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(action)
+    else:
+        form = LinkForm2()
+    return render(request, 'app_main/form.html', {'form': form, 'name': 'Dodaj link'})
+
+@login_required
+def change_password_view(request):
+    if request.method == "POST":
+        form = ChangePasswordForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = ChangePasswordForm(request.user)
+    return render(request, 'app_main/form.html', {'form': form, 'name': 'Zmień hasło'})
+
+@login_required
+def stats_view(request):
+    context = {
+        'links_number': len(Submission.objects.all()),
+        'links_without_the_cathegory': len(Submission.objects.filter(category=None)),
+        'links_with_the_cathegory': len(Submission.objects.exclude(category=None)),
+    }
+    return render(request, 'app_main/stats.html', context=context)
+
+@login_required
+def change_category_view(request, pk, category):
+    link = get_object_or_404(Submission, pk=pk)
+    link.category = SubmissionCategory.objects.get(id=category)
+    link.save()
+    return HttpResponse("done")
+
+
+@login_required
+def remove_category_view(request, pk):
+    link = get_object_or_404(Submission, pk=pk)
+    link.category = None
+    link.save()
+    return HttpResponse("not done")
+
+
+@login_required
+def lookup_view(request, phrase):
+    links = Submission.objects.filter(Q(link__icontains=phrase) | Q(platform__name__icontains=phrase)).order_by('link')
+    for link in links:
+        link.short_link = link.link[:50] + "..." if len(link.link) > 50 else link.link
+    return render(request, 'app_main/lookup.html', {'links': links, 'phrase': phrase})
+
+
+@login_required
+def export_view(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="links.txt"'
+    
+    output = ""
+    for link in Submission.objects.all():
+        if link.category is not None and link.category is not None:
+            output += link.link + "(" + link.platform.name + ", " + link.category.name + ")\n"
+    response.content = output
+    
+    return response
