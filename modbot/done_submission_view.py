2ca8aca87bdb76086928ca9ca842ed93ccacbdca
zardori <ignacy2038@gmail.com>
2024-06-09 13:00:56 +0200

    Added updating done views on discord, reacting to update on the webpage.

--- modbot/done_submission_view.py
+++ modbot/done_submission_view.py
@@ -10,12 +10,26 @@ class DoneSubmissionView:
         self.submission = submission
         self.controller = controller
         self.view_id = view_id
+        self.message = None
 
-    # for now no interactions
-    async def display(self, channel):
+    async def make_message_embed(self):
         submission_category = await get_submission_category(self.submission)
         embed = discord.Embed(title=f"Submission", description=self.submission.link,
                               color=discord.Color.blue())
         embed.add_field(name="Category", value=submission_category.name, inline=False)
-        await channel.send(embed=embed)
+        return embed
+
+    # for now no interactions
+    async def display(self, channel):
+        # submission_category = await get_submission_category(self.submission)
+        # embed = discord.Embed(title=f"Submission", description=self.submission.link,
+        #                       color=discord.Color.blue())
+        # embed.add_field(name="Category", value=submission_category.name, inline=False)
+        embed = await self.make_message_embed()
+        self.message = await channel.send(embed=embed)
         await channel.send("\u200B")
+
+    async def update(self, submission: Submission):
+        self.submission = submission
+        embed = await self.make_message_embed()
+        await self.message.edit(embed=embed)
